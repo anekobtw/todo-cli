126b9ea5a2cc4b60f2923019472866864b35599a
ssayand <krolexonder@gmail.com>
2023-12-10 11:55:20 +0400

    Update todo.cpp

--- todo.cpp
+++ todo.cpp
@@ -1,59 +1,77 @@
 #include <iostream>
-#include <list>
-
-using namespace std;
+#include <vector>
+#include <limits>
+#include <fstream>
+#include <conio.h>
 
 int main() {
-    list<string> tasks;
+    std::vector<std::string> tasks;
+
+    // Loading the tasks
+    std::ifstream Load("save.txt");
+    std::string task;
+    while (std::getline(Load, task)) {
+        tasks.push_back(task);
+    }
+    Load.close();
+
 
     while (true) {
-        cout << "Your tasks:\n";
-        int i = 0;
-        for (string task : tasks) {
-            cout << i << ". " << task << "\n"; // printing tasks
-            i++;
+        // Printing the tasks
+        std::cout << "Your tasks:\n";
+        for (size_t i = 0; i < tasks.size(); i++) {
+            std::cout << i + 1 << ". " << tasks[i] << "\n";
         }
 
+        // Asking what to do
         int whattodo;
-        cout << "\n\nType 1 to add a new task\nType 2 to remove a task\nType 3 to exit\n";
-        cin >> whattodo;
+        std::cout << "\n\nType 1 to add a new task\nType 2 to remove a task\nType 3 to save and exit\n";
+
+        while (!(std::cin >> whattodo) || (whattodo != 1 && whattodo != 2 && whattodo != 3)) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input. Please enter 1, 2, or 3: ";
+        }
 
         switch (whattodo) {
         case 1: {
             system("cls");
+            std::cin.ignore();
 
-            // get a new task
-            string task;
-            cout << "Enter the task: ";
-            cin.ignore();
-            getline(cin, task);
-            
-            // add a new task
+            std::string task;
+            std::cout << "Enter the task: ";
+            std::getline(std::cin, task);
             tasks.push_back(task);
-            
+
             system("cls");
             break;
         }
         case 2: {
             system("cls");
 
-            // get the index
             int taskIndex;
-            cout << "Enter the index of the task to remove: ";
-            cin >> taskIndex;
-            
-            // Check if the index is valid
-            if (taskIndex >= 0 && static_cast<size_t>(taskIndex) < tasks.size()) {
-                auto it = tasks.begin();
-                advance(it, taskIndex);
-                tasks.erase(it);
+            std::cout << "Enter the index of the task to remove: ";
+            std::cin >> taskIndex;
+            while(taskIndex < 1 || static_cast<size_t>(taskIndex) > tasks.size()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Invalid index. Please enter a valid index: ";
             }
+
+            tasks.erase(tasks.begin() + taskIndex - 1);
 
             system("cls");
             break;
         }
-        case 3:
+        case 3: {
+            std::ofstream Save;
+            Save.open("save.txt");
+            for (const std::string& task : tasks) {
+                Save << task << '\n';
+            }
+            Save.close();
             return 0;
+        }
         default:
             system("cls");
             break;
